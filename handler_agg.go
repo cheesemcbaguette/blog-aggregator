5bd6ae04ff19814944247f3a9e50091602b23b5a
CheeseMcBaguette <29225021+cheesemcbaguette@users.noreply.github.com>
2024-10-15 12:02:04 +0200

    Aggregate posts from feeds in db

--- handler_agg.go
+++ handler_agg.go
@@ -1,9 +1,58 @@
 package main
 
-import "context"
+import (
+	"context"
+	"example.com/username/blog-aggregator/cheese/internal/database"
+	"fmt"
+	"log"
+	"time"
+)
 
 func handlerAggregate(s *state, cmd command) error {
-	// Assuming feed URL is hardcoded for now
-	feedURL := "https://www.wagslane.dev/index.xml"
-	return aggregateFeed(context.Background(), s, feedURL)
+	// Parse duration
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("expected 1 argument: time_between_reqs")
+	}
+	timeBetweenRequests, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("invalid duration: %v", err)
+	}
+	fmt.Printf("Collecting feeds every %v\n", timeBetweenRequests)
+
+	// Setup ticker
+	ticker := time.NewTicker(timeBetweenRequests)
+	defer ticker.Stop()
+
+	for {
+		scrapeFeeds(s)
+		<-ticker.C
+	}
+}
+
+func scrapeFeeds(s *state) {
+	feed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		log.Println("Couldn't get next feeds to fetch", err)
+		return
+	}
+	log.Println("Found a feed to fetch!")
+	scrapeFeed(s.db, feed)
+}
+
+func scrapeFeed(db *database.Queries, feed database.Feed) {
+	err := db.MarkFeedFetched(context.Background(), feed.ID)
+	if err != nil {
+		log.Printf("Couldn't mark feed %s fetched: %v", feed.Name, err)
+		return
+	}
+
+	feedData, err := fetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		log.Printf("Couldn't collect feed %s: %v", feed.Name, err)
+		return
+	}
+	for _, item := range feedData.Channel.Item {
+		fmt.Printf("Found post: %s\n", item.Title)
+	}
+	log.Printf("Feed %s collected, %v posts found", feed.Name, len(feedData.Channel.Item))
 }
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -10,3 +10,13 @@ FROM feeds
 
 -- name: GetFeedByURL :one
 SELECT * FROM feeds WHERE url = $1 LIMIT 1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at = NOW(), updated_at = NOW()
+WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at NULLS FIRST, updated_at
+LIMIT 1;
